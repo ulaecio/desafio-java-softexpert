1de052024f47173d88036338f1f768a46f365489
ulaecio <ulaecio@live.com>
2023-09-28 10:45:42 -0300

    Project created

---
+++ backend/src/main/java/com/ulasoftware/billdivider/BillDividerApplication.java
@@ -0,0 +1,13 @@
+package com.ulasoftware.billdivider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillDividerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillDividerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ulasoftware/billdivider/BillDividerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulasoftware.billdivider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillDividerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
